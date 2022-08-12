55f20554ee6a0f9dbfbcf29263cdf26a94e895d5
Arye Eidelman <aryedov.e@gmail.com>
2022-08-12 13:14:54 -0700

    Move hooks to separate files

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,5 @@
-import React, { useEffect, useRef, useState } from 'react'
 import './App.css'
-
-
-export const useIsMount = () => {
-  const isMountRef = useRef(true)
-  useEffect(() => {
-    isMountRef.current = false
-  }, [])
-  return isMountRef.current
-}
-
-// a useState wrapper that syncs with local storage
-function useLocalStore<T>(defaultValue: any = {}, storeKey = "todo-playground") {
-  const [data, setData] = useState<T>(defaultValue)
-  const isMount = useIsMount()
-
-  // save state to localStorage
-  useEffect(() => {
-    if (!isMount) {
-      window.localStorage.setItem(storeKey, JSON.stringify(data))
-    }
-  }, [data, storeKey, isMount])
-
-  // restore state from localStorage
-  function loadFromLocalStorage() {
-    const storedData = window.localStorage.getItem(storeKey)
-    if (typeof storedData === 'string') {
-      setData(JSON.parse(storedData))
-    }
-  }
-
-  // when mounting
-  useEffect(loadFromLocalStorage, [storeKey])
-
-  // and in response to changes from other tabs
-  useEffect(() => {
-    window.addEventListener('storage', loadFromLocalStorage)
-    return () => window.removeEventListener('storage', loadFromLocalStorage)
-  })
-
-  return [data, setData] as const
-}
-
+import { useLocalStore } from './hooks'
 
 type Todo = {
   title: string,
@@ -68,10 +26,10 @@ function App() {
   }>({
     newTodo: { title: "", completed: false },
     todos: {
-      "626543": todoTemplate("demo 1"),
-      "708935": todoTemplate("demo 2"),
-      "765473": todoTemplate("demo 3"),
-      "778935": todoTemplate("demo 4")
+      "626543": todoTemplate("Apples"),
+      "708935": todoTemplate("Oranges"),
+      "765473": todoTemplate("Peppers"),
+      "778935": todoTemplate("Bread")
     }
   })
   const { newTodo, todos } = store
---
+++ src/hooks.ts
@@ -0,0 +1,42 @@
+
+import { useEffect, useRef, useState } from 'react'
+
+export const useIsMount = () => {
+  const isMountRef = useRef(true)
+  useEffect(() => {
+    isMountRef.current = false
+  }, [])
+  return isMountRef.current
+}
+
+// a useState wrapper that syncs with local storage
+export const useLocalStore = function <T>(defaultValue: any = {}, storeKey = "todo-playground") {
+  const [data, setData] = useState<T>(defaultValue)
+  const isMount = useIsMount()
+
+  // save state to localStorage
+  useEffect(() => {
+    if (!isMount) {
+      window.localStorage.setItem(storeKey, JSON.stringify(data))
+    }
+  }, [data, storeKey, isMount])
+
+  // restore state from localStorage
+  function loadFromLocalStorage() {
+    const storedData = window.localStorage.getItem(storeKey)
+    if (typeof storedData === 'string') {
+      setData(JSON.parse(storedData))
+    }
+  }
+
+  // when mounting
+  useEffect(loadFromLocalStorage, [storeKey])
+
+  // and in response to changes from other tabs
+  useEffect(() => {
+    window.addEventListener('storage', loadFromLocalStorage)
+    return () => window.removeEventListener('storage', loadFromLocalStorage)
+  })
+
+  return [data, setData] as const
+}
