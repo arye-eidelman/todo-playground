9f8b6495105f7686427df14dc8c0562e05e4e3d9
Arye Eidelman <aryedov.e@gmail.com>
2022-08-15 18:14:12 -0700

    Add tailwind and some layout styles

--- src/App.tsx
+++ src/App.tsx
@@ -12,9 +12,10 @@ function shortTitle(title: string) {
 type Todo = {
   title: string,
   completed: boolean,
-  createdAt?: string,
-  updatedAt?: string,
-  deletedAt?: string | null
+  createdAt: string,
+  updatedAt: string,
+  deletedAt?: string,
+  dueAt?: string
 }
 
 function todoTemplate(title = ""): Todo {
@@ -23,7 +24,6 @@ function todoTemplate(title = ""): Todo {
     completed: false,
     createdAt: new Date().toISOString(),
     updatedAt: new Date().toISOString(),
-    deletedAt: null
   }
 }
 
@@ -43,27 +43,28 @@ function App() {
   const { newTodo, todos } = store
 
   return (
-    <div className="App">
+    <div className="mx-auto max-w-xl">
       <header>
         <h1>Todo Playground</h1>
       </header>
       <hr />
 
-      <main>
+      <main className='mx-auto max-w-md'>
         <h3>Todos</h3>
-        <ul style={{ listStyleType: "none", padding: 0 }}>
+        <ul className="list-none p-0">
           {Object.keys(todos).map((key, index) => {
             const todo = todos[key]
             return (
-              <li key={key} style={{ margin: "4px 0" }}>
+              <li key={key} className="my-3 space-x-2 flex">
                 <input
                   type="checkbox"
-                  title={`Mark ${todo.completed ? 'un': ''}completed todo-item '${shortTitle(todo.title)}'`}
+                  title={`Mark ${todo.completed ? 'un' : ''}completed todo-item '${shortTitle(todo.title)}'`}
                   checked={todo.completed}
                   onChange={e => updateStore({ ...store, todos: { ...todos, [key]: { ...todo, completed: e.target.checked } } })}
                 />
                 <input
                   type="text"
+                  className='w-full'
                   title="Todo-item title"
                   value={todo.title}
                   onChange={e => {
@@ -71,22 +72,23 @@ function App() {
                   }}
                 />
                 <button
-                title={`Trash todo-item '${shortTitle(todo.title)}'`}
-                onClick={() => {
-                  const nextTodos = { ...todos }
-                  delete nextTodos[key]
-                  updateStore({ ...store, todos: nextTodos })
-                }}>
+                  title={`Trash todo-item '${shortTitle(todo.title)}'`}
+                  onClick={() => {
+                    const nextTodos = { ...todos }
+                    delete nextTodos[key]
+                    updateStore({ ...store, todos: nextTodos })
+                  }}>
                   🗑
                 </button>
               </li>
             )
           })}
 
-          <li>
+          <li className='my-3 space-x-2 flex'>
             <input type="checkbox" disabled />
             <input
               type="text"
+              className='w-full'
               title="New todo-item title"
               placeholder='I need to _________'
               value={newTodo.title}
